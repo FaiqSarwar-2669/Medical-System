9d117607f034493221383277bdbebb20a52b9dca
AbdurRehman-236 <ar8342090@gmail.com>
2023-12-02 15:20:37 +0500

    add user

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-tab1',
   templateUrl: './tab1.page.html',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab1Page implements OnInit {
 
-  constructor() { }
+  constructor(private router :Router) {   }
 
   ngOnInit() {
   }
-
+  login(){
+    this.router.navigate(['/home'])
+  }
+  signup(){
+    // console.log('hello')
+    this.router.navigate(['/tab2'])
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera, CameraResultType,CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-tab2',
@@ -6,10 +7,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tab2.page.scss'],
 })
 export class Tab2Page implements OnInit {
-
+form:any={
+  'fullname':'',
+  'nickname':'',
+  'dateofbirth':'',
+  'email':'',
+  'gender':'',
+  'password':'',
+  'confirmpassword':'',
+  'image':'',
+}
   constructor() { }
 
   ngOnInit() {
   }
+  signup(){
+    if(this.form.fullname.trim() === ''){
+      console.log('Enter Full Name')
+    }else if(this.form.nickname.trim() === ''){
+      console.log('Enter Nick Name')
+    }else if(this.form.dateofbirth.trim() === '' ){
+      console.log('Enter date of birth')
+    }else if(this.form.email.trim() === '' ){
+      console.log('Enter email')
+    }else if(this.form.gender.trim() === ''){
+      console.log('Enter gender')
+    }else if(this.form.password.trim() === '' ){
+      console.log('Enter Password')
+    }else if(this.form.confirmpassword.trim() === ''){
+      console.log('Enter confirm password')
+    }else if(this.form.password.trim() === this.form.confirmpassword.trim()){
+      console.log(this.form)
+    }
+  }
+  async open(){
+    console.log('helo')
+      const image = await Camera.getPhoto({
+        quality: 100,
+        allowEditing: false,
+        resultType: CameraResultType.Uri,
+        source: CameraSource.Prompt 
+      });
+    
+      var imageUrl = image.webPath;
+      console.log(imageUrl)
 
+  }
 }
