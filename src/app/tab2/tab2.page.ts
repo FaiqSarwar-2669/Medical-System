658a8745075da9af9dd67424e56a32d989bc02a1
FaiqSarwa-2669 <code.faiq786@gmail.com>
2023-12-05 10:25:56 +0500

    add image functionality

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Camera, CameraResultType,CameraSource } from '@capacitor/camera';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-tab2',
@@ -7,50 +7,57 @@ import { Camera, CameraResultType,CameraSource } from '@capacitor/camera';
   styleUrls: ['./tab2.page.scss'],
 })
 export class Tab2Page implements OnInit {
-form:any={
-  'fullname':'',
-  'nickname':'',
-  'dateofbirth':'',
-  'email':'',
-  'gender':'',
-  'password':'',
-  'confirmpassword':'',
-  'image':'',
-}
+  form: any = {
+    'fullname': '',
+    'nickname': '',
+    'dateofbirth': '',
+    'email': '',
+    'gender': '',
+    'password': '',
+    'confirmpassword': '',
+    'image': '',
+  }
+  frontimage:any
   constructor() { }
 
   ngOnInit() {
   }
-  signup(){
-    if(this.form.fullname.trim() === ''){
+  signup() {
+    if (this.form.fullname.trim() === '') {
       console.log('Enter Full Name')
-    }else if(this.form.nickname.trim() === ''){
+    } else if (this.form.nickname.trim() === '') {
       console.log('Enter Nick Name')
-    }else if(this.form.dateofbirth.trim() === '' ){
+    } else if (this.form.dateofbirth.trim() === '') {
       console.log('Enter date of birth')
-    }else if(this.form.email.trim() === '' ){
+    } else if (this.form.email.trim() === '') {
       console.log('Enter email')
-    }else if(this.form.gender.trim() === ''){
+    } else if (this.form.gender.trim() === '') {
       console.log('Enter gender')
-    }else if(this.form.password.trim() === '' ){
+    } else if (this.form.password.trim() === '') {
       console.log('Enter Password')
-    }else if(this.form.confirmpassword.trim() === ''){
+    } else if (this.form.confirmpassword.trim() === '') {
       console.log('Enter confirm password')
-    }else if(this.form.password.trim() === this.form.confirmpassword.trim()){
+    } else if (this.form.password.trim() === this.form.confirmpassword.trim()) {
       console.log(this.form)
     }
   }
-  async open(){
+  open = async () => {
     console.log('helo')
-      const image = await Camera.getPhoto({
-        quality: 100,
-        allowEditing: false,
-        resultType: CameraResultType.Uri,
-        source: CameraSource.Prompt 
-      });
-    
-      var imageUrl = image.webPath;
-      console.log(imageUrl)
-
+    const image = await Camera.getPhoto({
+      quality: 100,
+      allowEditing: true,
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Prompt
+    });
+    this.frontimage = image.webPath
+    this.form.image= await this.convertImageToFile(this.frontimage)
+  }
+  async convertImageToFile(imagePath: string){
+    const response = await fetch(imagePath);
+    const blob = await response.blob();
+  
+    const fileName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
+    const file = new File([blob], fileName, { type: blob.type });
+    return file;
   }
 }
