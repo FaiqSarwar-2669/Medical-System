dc933638c5f05defff8cb8c26c29ff017722344c
FaiqSarwa-2669 <code.faiq786@gmail.com>
2023-12-08 00:50:22 +0500

    complete doctor page

--- src/app/doctorlist/doctorlist.page.ts
+++ src/app/doctorlist/doctorlist.page.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DoctorlistPage implements OnInit {
 
+  isBottom: boolean = false;
+  isFilterClicked: boolean = true;
   constructor() { }
 
   ngOnInit() {
   }
 
+  filter(event: Event){
+    this.isBottom = !this.isBottom;
+    this.isFilterClicked = !this.isFilterClicked;
+  }
 }
